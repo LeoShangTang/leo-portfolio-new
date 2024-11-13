fb6d007d5783c735be80be1aa0eeed4384398a0b
Leo <leoshang12@gmail.com>
2024-11-13 10:58:14 -0800

    added images for song rec section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
-import Summary from "@/components/aboutme/Summary";
+import MusicRecs from "@/components/aboutmepage/music-rec-section/SongList";
+import Summary from "@/components/aboutmepage/Summary";
 
 export default function About() {
   return (
     <div className="flex flex-col items-center mx-auto min-h-screen max-w-[1280px] mt-28">
-      <Summary />;
+      <Summary />
+      <MusicRecs />
     </div>
   );
 }
---
+++ src/components/aboutmepage/music-rec-section/SongList.jsx
@@ -0,0 +1,11 @@
+export default function MusicRecs() {
+  return (
+    <div className="flex justify-center items-center w-[90%]">
+      <h2 className="text-sub-title-xl font-IBMMedium">
+        I love <span className="font-bold whitespace-nowrap">music</span>! Here
+        are some of my{" "}
+        <span className="font-bold whitespace-nowrap">favorite songs</span>!
+      </h2>
+    </div>
+  );
+}
