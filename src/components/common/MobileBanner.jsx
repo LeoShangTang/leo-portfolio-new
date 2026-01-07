ddfa52ec6e597ddba82a6baf2b8f97d7f58f4a3a
Leo <leoshang12@gmail.com>
2026-01-07 12:03:53 -0800

    resume link updated

--- src/components/common/MobileBanner.jsx
+++ src/components/common/MobileBanner.jsx
@@ -83,7 +83,7 @@ export default function MobileBanner() {
               About Me
             </Link>
             <a
-              href="https://drive.google.com/file/d/1tejVYfIfNaG9MtQx6fwCBLotd5OgTMXw/view?usp=sharing"
+              href="https://drive.google.com/file/d/1wP6ILnNkMcc2qqXfGUKcfbBPH_6PKh16/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="font-IBMMedium text-body-xl px-6 py-1 rounded-full text-gray-700 hover:bg-gray-100
